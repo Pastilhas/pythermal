b87efbfb0ec38d03330705265ac20017a3534bfd
João Campos <joao.campos1221@gmail.com>
2024-06-28 17:13:13 +0100

    move code to class

--- src/main.py
+++ src/main.py
@@ -9,38 +9,21 @@ parser.add_argument("device", type=int, nargs="?", default=0)
 args = parser.parse_args()
 
 dev = args.device
-cap = cv.VideoCapture(dev)
-cap.set(cv.CAP_PROP_CONVERT_RGB, 0)
+vid = video.Video(dev)
 
-min = 10
-max = 60
-brightness = 0.00
-contrast = 0.95
-w = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-h = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)) // 2
-cv.namedWindow("preview", cv.WINDOW_GUI_NORMAL)
-cv.resizeWindow("preview", w, h)
-
-ret, frame = cap.read()
-while ret:
-    frame = video.raw(frame, contrast, brightness, min, max)
-    cv.imshow("preview", frame["img"])
-
+while vid.show():
     keyPress = cv.waitKey(1)
     if keyPress == ord("q"):
         break
 
     if keyPress == ord("a"):
-        brightness += 0.05
+        vid.brightness += 0.05
     if keyPress == ord("d"):
-        brightness -= 0.05
+        vid.brightness -= 0.05
 
     if keyPress == ord("w"):
-        contrast += 0.05
+        vid.contrast += 0.05
     if keyPress == ord("s"):
-        contrast -= 0.05
+        vid.contrast -= 0.05
 
-    ret, frame = cap.read()
-
-cap.release()
-cv.destroyAllWindows()
+vid.close()
--- src/video.py
+++ src/video.py
@@ -1,13 +1,41 @@
 import numpy as np
+import cv2 as cv
 
 
-def raw(frame, contrast, brightness, min, max):
-    r = np.reshape(frame[0], (2, 192, 256, 2))
-    r = r[1, :, :, :].astype(np.intc)
-    r = (r[:, :, 1] << 8) + r[:, :, 0]
-    r = r / 64 - 273
+class Video:
+    min_temp = 10
+    max_temp = 60
+    brightness = 0.00
+    contrast = 1.00
 
-    img = r
-    img = (img - min) / (max - min)
-    img = img * contrast + brightness
-    return {"min": r.min(), "max": r.max(), "mean": r.mean(), "img": img, "temp": r}
+    def __init__(self, device: int) -> None:
+        self.cap = cv.VideoCapture(device)
+        self.cap.set(cv.CAP_PROP_CONVERT_RGB, 0)
+        self.w = int(self.cap.get(cv.CAP_PROP_FRAME_WIDTH))
+        self.h = int(self.cap.get(cv.CAP_PROP_FRAME_HEIGHT)) // 2
+        cv.namedWindow("preview", cv.WINDOW_GUI_NORMAL)
+        cv.resizeWindow("preview", self.w, self.h)
+
+    def close(self):
+        self.cap.release()
+        cv.destroyAllWindows()
+
+    def show(self) -> bool:
+        ret, frame = self.cap.read()
+        if ret:
+            frame = self.transform(frame)
+            cv.imshow("preview", frame["img"])
+        return ret
+
+    def transform(self, frame):
+        r = np.reshape(frame[0], (2, 192, 256, 2))  # separate image and data
+        r = r[1, :, :, :].astype(np.intc)  # get data
+        r = (r[:, :, 1] << 8) + r[:, :, 0]  # rearrange values
+        r = r / 64 - 273  # value to degree Celsius
+
+        img = r.copy()  # clone
+        img = (img - self.min_temp) / (
+            self.max_temp - self.min_temp
+        )  # normalize with [min, max]
+        img = img * self.contrast + self.brightness  # add contrast and brightness
+        return {"min": r.min(), "max": r.max(), "mean": r.mean(), "img": img, "temp": r}
