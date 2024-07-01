78fd87b37f40defb3869232105d0105f723cd489
João Campos <joao.campos1221@gmail.com>
2024-07-01 16:13:31 +0100

    add text to window

--- src/window.py
+++ src/window.py
@@ -6,6 +6,8 @@ class Window:
 
     def __init__(self, w, h):
         self.name = f"preview{Window.uid}"
+        self.w = w
+        self.h = h
         cv.namedWindow(self.name, cv.WINDOW_GUI_NORMAL)
         cv.resizeWindow(self.name, w, h)
         Window.uid += 1
@@ -15,6 +17,16 @@ class Window:
 
     def show(self, frame):
         img = frame * self.contrast + self.brightness
+        cv.putText(
+            img,
+            f"{self.brightness:.1f} {self.contrast:.1f}",
+            (0, self.h + 1),
+            cv.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (255, 255, 255),
+            1,
+            cv.LINE_AA,
+        )
         cv.imshow(self.name, img)
 
     def parse(self, key):
