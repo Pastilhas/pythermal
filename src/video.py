0c0151eee4d497669c3ae32fdc0584e43f8bec86
João Campos <joao.campos1221@gmail.com>
2024-07-01 15:42:07 +0100

    refactor for new classes

--- src/video.py
+++ src/video.py
@@ -1,37 +1,37 @@
 import numpy as np
 import cv2 as cv
 
+from recorder import Recorder
+from window import Window
+
 
 class Video:
-    recording = False
-    n_frame = 0
     min_temp = 10
     max_temp = 60
-    brightness = 0.00
-    contrast = 1.00
 
     def __init__(self, device: int, output: str = "") -> None:
+        self.window: Window | None = None
+        self.recorder: Recorder | None = None
         self.cap = cv.VideoCapture(device)
         self.cap.set(cv.CAP_PROP_CONVERT_RGB, 0)
         self.w = int(self.cap.get(cv.CAP_PROP_FRAME_WIDTH))
         self.h = int(self.cap.get(cv.CAP_PROP_FRAME_HEIGHT)) // 2
         self.out = output
-        cv.namedWindow("preview", cv.WINDOW_GUI_NORMAL)
-        cv.resizeWindow("preview", self.w, self.h)
 
     def close(self):
         self.cap.release()
-        cv.destroyAllWindows()
 
     def show(self) -> bool:
         ret, frame = self.cap.read()
         if ret:
             _, norm = self.transform(frame)
-            cv.imshow("preview", norm)
-            if self.out and self.recording:
-                path = f"{self.out}/{self.n_frame}.bmp"
-                cv.imwrite(path, norm * 255)
-                self.n_frame += 1
+
+            if self.window:
+                self.window.show(norm)
+
+            if self.recorder:
+                self.recorder.show(norm)
+
         return ret
 
     def transform(self, frame):
@@ -40,9 +40,8 @@ class Video:
         raw = (raw[:, :, 1] << 8) + raw[:, :, 0]  # rearrange values
         raw = (raw >> 6) - 273  # value to degree Celsius
 
-        img = raw.copy()  # clone
-        img = img.clip(self.min_temp, self.max_temp)
-        img = img - self.min_temp
-        img = img / (self.max_temp - self.min_temp)  # normalize with [min, max]
-        img = img * self.contrast + self.brightness  # add contrast and brightness
-        return raw, img
+        norm = raw.copy()  # clone
+        norm = norm.clip(self.min_temp, self.max_temp)
+        norm = norm - self.min_temp
+        norm = norm / (self.max_temp - self.min_temp)  # normalize with [min, max]
+        return raw, norm
