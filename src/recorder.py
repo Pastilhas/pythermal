3f702be548eb5e384ae7c8f64ba1702986472a06
João Campos <joao.campos1221@gmail.com>
2024-07-02 14:34:10 +0100

    set frame name as numbers

--- src/recorder.py
+++ src/recorder.py
@@ -1,14 +1,18 @@
-from datetime import datetime
 import cv2 as cv
 import os
 
 
 class Recorder:
     def __init__(self, output_folder):
+        self.n_frame = 0
         self.output_folder = output_folder
         os.makedirs(output_folder, exist_ok=True)
 
-    def save_frame(self, frame):
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
-        filename = f"{self.output_folder}/{timestamp}.png"
+    def save_frame(self, frame, is_norm=False):
+        filename = f"{self.output_folder}/{self.n_frame:03}.png"
+        self.n_frame += 1
+
+        if is_norm:
+            frame = frame * 255
+
         cv.imwrite(filename, frame)
